a3503338def0824f7d68142fc8026c3408161070
vlasiuksasha <vlasukoleksandra3@gmail.com>
2025-08-23 19:44:42 +0300

    fix some issues

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,24 +8,27 @@ test('Check that site has a title', async ({ page }) => {
     await expect(page).toHaveTitle('Login - Practice Software Testing - Toolshop - v5.0');
   });
 
-test('Fill in the fields on the login page', async ({ page }) => {
-
+  test('Fill in the fields on the login page', async ({ page }) => {
     const emailTest = 'customer@practicesoftwaretesting.com'
-    const emailField = page.getByTestId('email')
-    await expect(emailField).toBeVisible();
-    await emailField.fill(emailTest);
-    
-    const passwordTest = 'welcome01'
-    const passwordField = page.getByTestId('password')
-    await passwordField.fill(passwordTest);
-    await expect(passwordField).toHaveValue(passwordTest);
-
+    const passwordTest = 'welcome01' 
+  
+    // Playwright сам зачекає, поки поле стане visible + enabled
+    await page.getByTestId('email').fill(emailTest);
+    await page.getByTestId('password').fill(passwordTest);
+  
+    // Цей чек має сенс, бо ми хочемо переконатися,
+    // що справді ввели потрібний текст
+    await expect(page.getByTestId('password')).toHaveValue(passwordTest);
+  
+    // Те саме з кнопкою: Playwright зачекає до "enabled", але тут 
+    // ми явно хочемо перевірити стан кнопки
     const loginButton = page.getByTestId('login-submit');
-    await expect(loginButton).toBeVisible();
-    await expect(loginButton).toBeEnabled()
+    await expect(loginButton).toBeEnabled();
     await loginButton.click();
-
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account')
-    await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe')
-    await expect(page.getByTestId('page-title')).toContainText('My account')
+  
+    // Assertions після переходу на іншу сторінку
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+    await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
+    await expect(page.getByTestId('page-title')).toContainText('My account');
   });
+  
