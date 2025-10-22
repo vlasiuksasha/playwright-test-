3327dfd3220683d978d2be61782f0714b0a964a8
vlasiuksasha <vlasukoleksandra3@gmail.com>
2025-10-22 14:20:54 +0300

    update filer product test

--- tests/filter-product-by-category.spec.ts
+++ tests/filter-product-by-category.spec.ts
@@ -1,12 +1,20 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures';
 
+enum PowerTools {
+  Grinder = 'Grinder',
+  Sander = 'Sander',
+  Saw = 'Saw',
+  Drill = 'Drill',
+}
+
 
 test('Verify that products are filtered by category (Sander)', async ({ page, app }) => {
     await page.goto('/');
-    await app.homePage.filterBySubCategory('Sander')
-    const productNames = await app.homePage.getProductNames();
-    for (const name of productNames) {
-      expect(name.toLowerCase()).toContain('sander');
+    await app.homePage.checkAndWaitforResponse();
+
+    const productNames = await app.homePage.getAllTextContents(app.homePage.productName);
+    for (const i of productNames) {
+      expect(i).toContain(PowerTools.Sander);
     }
   });
