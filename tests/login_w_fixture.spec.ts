00d28601fcbc8793e3c309e1236e699c7e9d58c9
vlasiuksasha <vlasukoleksandra3@gmail.com>
2025-10-22 17:35:40 +0300

    update filer product test

--- tests/login_w_fixture.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from '@playwright/test';
-import { test } from '../fixtures';
-
-test('Verify successful login', async ({ app, loggedInApp }) => {
-  await expect(loggedInApp.page).toHaveURL(/\/account/);
-  await expect(app.loginPage.header.navMenu).toContainText('Jane Doe');
-  await expect(app.accountPage.titlePage).toContainText('My account');
-});
